b0f309f14a414e117d2ea6b1edeef5489d51e026
Jethro May <6702147+jethromayuk@users.noreply.github.com>
2022-02-17 14:04:42 +0200

    Update main.go

--- update/main.go
+++ update/main.go
@@ -30,7 +30,7 @@ func getRSS(rssFeed string) ([]string, error) {
 
 func makeReadme(filename string) error {
 	fp := gofeed.NewParser()
-	feed, err := fp.ParseURL("https://jethromay.com/rss.xml")
+	feed, err := fp.ParseURL("https://jethromay.dev/rss.xml")
 	if err != nil {
 		log.Fatalf("error getting feed: %v", err)
 	}
